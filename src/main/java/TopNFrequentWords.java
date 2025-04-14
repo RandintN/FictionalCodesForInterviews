46198cc43c865d7bb2825a21337dc3ea13bd4198
Robson Cassiano <robson.cassiano@pm.me>
2025-04-14 18:09:49 -0300

    Blah

--- src/main/java/TopNFrequentWords.java
+++ src/main/java/TopNFrequentWords.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TopNFrequentWords {
+    public static List<String> findTopNFrequentWords(List<String> wordsToBeChecked, int topMost) {
+        final var lowerCaseWords = wordsToBeChecked.stream().map(String::toLowerCase).toList();
+
+        lowerCaseWords.stream().sorted().distinct().limit(topMost).toList();
+
+        return lowerCaseWords.stream()
+                .filter(Objects::nonNull)
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Comparator
+                        .comparing(Map.Entry<String, Long>::getValue, Comparator.reverseOrder())
+                        .thenComparing(Map.Entry::getKey))
+                .limit(topMost)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/TopNFrequentWordsTest.java
+++ src/test/java/TopNFrequentWordsTest.java
@@ -1,4 +1,41 @@
-public class TopNFrequentWords {
-    @Test
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TopNFrequentWordsTest {
+    @Test void
+    should_have_top_N_as_the_size_of_the_list() {
+        // Arrange
+        List<String> wordsToBeChecked = Arrays.asList("Java", "Javascript",
+        "java", "C++", "javaScript", "Groovy", "jAvA", "abap","ABAP", "aBaP");
+        int topMost = 3;
+
+        // Act
+        final var actualResult = TopNFrequentWords.findTopNFrequentWords(wordsToBeChecked, topMost); // Class Under Test
+
+        // Assert
+        assertThat(actualResult).hasSize(3);
+    }
+
+    @Test void
+    should_have_most_frequently_words_on_the_list() {
+        // Arrange
+        List<String> wordsToBeChecked = Arrays.asList("Java", "Javascript", "java", "C++", "javaScript", "Groovy", "jAvA", "abap","ABAP", "aBaP");
+        int topMost = 3;
+
+        // Act
+        final var actualResult = TopNFrequentWords.findTopNFrequentWords(wordsToBeChecked, topMost); // Class Under Test
+
+        // Assert
+        assertThat(actualResult)
+                .satisfiesExactly(word -> assertThat(word).isEqualTo("abap"),
+                                 word2 -> assertThat(word2).isEqualTo("java"),
+                                 word3 -> assertThat(word3).isEqualTo("javascript"));
+    }
 
 }
